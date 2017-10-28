37a0b1724f394a20ed1b4cd4a2658876a081ab50
Prashan Welipitiya <prshn.weli@gmail.com>
2017-10-28 12:42:32 -0700

    added files

---
+++ config/connection.js
@@ -0,0 +1,31 @@
+// Set up MySQL connection.
+var mysql = require("mysql");
+
+// var connect = require('./connect.js')
+
+// var connection = mysql.createConnection(connect.input);
+
+if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else{
+    connection = mysql.createConnection({
+        root: 3000,
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: '',
+    });
+};
+
+
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
