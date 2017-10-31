026e435ec7788f2ce26c19726affcdcf7b73c513
G I <tronnie@Gs-MacBook-Pro.local>
2017-10-31 09:39:09 -0700

    updating connection.js

--- config/connection.js
+++ config/connection.js
@@ -9,11 +9,11 @@ if(process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else{
     connection = mysql.createConnection({
-        root: 3306,
+        port: 3306,
         host: 'localhost',
         user: 'root',
         password: '',
-        database: '',
+        database: 'project2_db',
     });
 };
 
