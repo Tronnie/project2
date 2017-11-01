0b655a815efe4fc72c4a7ec90dc2a1b6d50a0ac1
G I <tronnie@Gs-MacBook-Pro.local>
2017-11-01 09:44:48 -0700

    folder clean up

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,3 +5,4 @@ INSERT INTO userInfo (userName, password, userWeight) VALUES ('Abdi',444, 444);
 
 INSERT INTO userWorkout (userName, excerciseName, weightAmount, repetitions, sets) VALUES ('Gail', 'biceps curls', 3, 12, 3);
 INSERT INTO userWorkout (userName, excerciseName, weightAmount, repetitions, sets) VALUES ('Abdi', 'leg abduction', 100, 12, 3);
+INSERT INTO userWorkout (userName, excerciseName, weightAmount, repetitions, sets) VALUES ('Said', 'chest press', 110, 12, 3);
